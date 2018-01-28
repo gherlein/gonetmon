caba1ef1cc5dc8e42679141b0619bd7665865455
Greg Herlein <gherlein@herlein.com>
2018-01-27 21:28:55 -0800

    added prometheus support

--- main.go
+++ main.go
@@ -13,9 +13,12 @@ import (
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"github.com/onsi/gocleanup"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"math"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -30,24 +33,33 @@ type node struct {
 }
 
 var (
-	device string = "eth0"
-	cidr   string = "192.168.1.1/24"
-	//	mask        net.IPMask
-	//	masklen     int
-	//	numhosts    int
-	//	baseaddr    string
-	snapshotLen int32 = 1024
-	promiscuous bool  = true
+	device      string = ""
+	cidr        string = ""
+	snapshotLen int32  = 1024
+	promiscuous bool   = true
 	err         error
 	timeout     time.Duration = 30 * time.Second
 	handle      *pcap.Handle
 	nodes       []node
-	debug       bool = true
+	debug       bool = false
+	netBytes         = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "network_bytes_total",
+		Help: "Number of bytes seen on the network.",
+	})
+	nodeBytes = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "node_bytes_total",
+			Help: "Number of bytes seen on that network node.",
+		},
+		[]string{"device"},
+	)
 )
 
 func init() {
-	flag.StringVar(&device, "device", "eth0", "name of the network device to monitor")
-	flag.StringVar(&cidr, "cidr", "192.168.1.0/24", "CIDR of the network device to monitor")
+	flag.StringVar(&device, "device", "", "name of the network device to monitor")
+	flag.StringVar(&cidr, "cidr", "", "CIDR of the network device to monitor")
+	prometheus.MustRegister(netBytes)
+	prometheus.MustRegister(nodeBytes)
 }
 
 func calcNetwork(d string, c string) (int, string, error) {
@@ -86,6 +98,10 @@ func main() {
 	)
 
 	flag.Parse()
+	if device == "" || cidr == "" {
+		fmt.Printf("device name and/or cidr not specified\n")
+		os.Exit(3)
+	}
 	numhosts, baseaddr, err = calcNetwork(device, cidr)
 	fmt.Printf("Device: %s - CIDR: %s - numhosts: %d - baseaddr: %s\n", device, cidr, numhosts, baseaddr)
 
@@ -116,8 +132,14 @@ func main() {
 				fmt.Printf("%s - %s\n", addr, hostname)
 			}
 		}
+
 		nodes = append(nodes,
-			node{IP: net.ParseIP(addr), addr: addr, hostname: hostname, incount: 0, outcount: 0})
+			node{IP: net.ParseIP(addr),
+				addr:     addr,
+				hostname: hostname,
+				incount:  0,
+				outcount: 0,
+			})
 	}
 
 	if debug {
@@ -136,11 +158,17 @@ func main() {
 		}
 	}()
 
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		//printPacketInfo(packet)
 		analyzePacket(packet)
 	}
+
 }
 
 func analyzePacket(packet gopacket.Packet) {
@@ -148,16 +176,18 @@ func analyzePacket(packet gopacket.Packet) {
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	if ipLayer != nil {
 		ip, _ := ipLayer.(*layers.IPv4)
-
+		netBytes.Add(float64(ip.Length))
 		for i, node := range nodes {
 			if node.addr != "" {
 				if node.IP.Equal(ip.SrcIP) {
+					nodeBytes.With(prometheus.Labels{"device": node.hostname}).Add(float64(ip.Length))
 					nodes[i].outcount += uint64(ip.Length)
 					if debug {
 						fmt.Printf("From %s to %s - len %d\n", node.hostname, ip.DstIP, ip.Length)
 					}
 				}
 				if node.IP.Equal(ip.DstIP) {
+					nodeBytes.With(prometheus.Labels{"device": node.hostname}).Add(float64(ip.Length))
 					nodes[i].incount += uint64(ip.Length)
 					if debug {
 						fmt.Printf("From %s to %s - len %d\n", ip.SrcIP, node.hostname, ip.Length)
